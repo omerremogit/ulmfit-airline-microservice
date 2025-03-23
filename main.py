a6a3553da401e27604c62be4abbb81d18c3ab697
omerremogit <mohammed.omer1996@gmail.com>
2025-03-22 23:42:45 -0400

    Add dynamic model download from Google Drive

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastai.text.all import *
+import os
+import requests
 
 app = FastAPI()
-learn = load_learner('ulmfit_airline_model.pkl')
+
+# Download model from Google Drive if not present
+model_path = 'ulmfit_airline_model.pkl'
+if not os.path.exists(model_path):
+    print("Downloading model file...")
+    url = 'https://drive.google.com/uc?export=download&id=1cdZScmtkKCT7c_1I9KaGXQWLr7qDAj0n'
+    r = requests.get(url)
+    with open(model_path, 'wb') as f:
+        f.write(r.content)
+
+learn = load_learner(model_path)
 
 class TextInput(BaseModel):
     text: str
